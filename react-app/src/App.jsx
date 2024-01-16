a93464248c968de2636f18a5e413a8d8b0dd64bb
gary <github@accu.ca>
2024-01-15 17:19:27 -0800

    Update

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <h2>Hello React Class</h2>
-      {count} <button onClick={() => setCount(count + 1)}>+</button>
+      {count}
+       <button onClick={() => setCount(count + 1)}>+</button>
+       <button onClick={() => setCount(0)}>0</button>
     </div>
   );
 }
